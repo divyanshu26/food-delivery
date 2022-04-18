b1f5b410cad2a2d0b379e0cd19594251cb52c621
divyanhu <agarwaldivyanshu26@gmail.com>
2022-04-18 22:33:52 +0530

    removed unused code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Header from './components/Layout/Header';
 function App() {
   console.log('app');
   const [cartVisible,setCartVisible] = useState(false);
-  const [message, setMessage] = useState('venusq');
   function showCart(){
     setCartVisible(true);
   };
@@ -20,20 +19,11 @@ function App() {
     console.log('close');
     setCartVisible(false);
   };
-
-  function chngState(){
-    console.log('click');
-    setMessage(m=>{
-      console.log('inside');
-      return 'venusq1';
-    });
-  };
   return (
     
     <CartContextProvider>
       {cartVisible && <Cart hideCart ={hideCart}/>}
       <Header showCart={showCart}/>
-      <button onClick={chngState}>change state</button>
       <main>
         <Meals />
       </main>
